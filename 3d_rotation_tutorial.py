482be4b2bb296e8ea3b11416eb420c67e4ceca0e
Joe Kessler <copperbotte@gmail.com>
2025-06-11 21:57:33 -0700

    SQUASH LATER: svg testing commit

--- 3d_rotation_tutorial.py
+++ 3d_rotation_tutorial.py
@@ -153,7 +153,7 @@ def SVG_FROM_TEX(TEX_INPUT, scale=1.25, inline=False, nodiv=False):
         #return f' ![{svg}]({svg}) '
         #return f'<img src="{svg}" style="vertical-align: middle; display: inline-block;"/>'
         #return f'<img src="{svg}" style="height=1em; vertical-align: -1.5em; display: inline-block;"/>'
-        return f'<img src="{svg}" style="display: inline-block;"/>'
+        return f' <img src="{svg}" style="display: inline-block;"/> '
         result = f"""<div style="display: inline-block;"> 
 
 <img src="{svg}"/>
