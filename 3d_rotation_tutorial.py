9c65d5a6481c3f4735785dcf6ad7590499730afd
Joe Kessler <copperbotte@gmail.com>
2025-06-11 21:54:23 -0700

    SQUASH LATER: svg testing commit

--- 3d_rotation_tutorial.py
+++ 3d_rotation_tutorial.py
@@ -75,7 +75,7 @@ def SVG_FROM_TEX(TEX_INPUT, scale=1.25, inline=False, nodiv=False):
     
     if True:
 
-        if True:
+        if False:
             Call(cmd1, silence=True)
             print('#'*80)
             Call(cmd2, silence=True)
@@ -150,7 +150,8 @@ def SVG_FROM_TEX(TEX_INPUT, scale=1.25, inline=False, nodiv=False):
 
     # The style applied here forces the svgs to stay inline with the text.
     if inline:
-        return f'<img src="{svg}" style="vertical-align: middle; display: inline-block;"/>'
+        return f' ![{svg}]({svg}) '
+        #return f'<img src="{svg}" style="vertical-align: middle; display: inline-block;"/>'
         #return f'<img src="{svg}" style="height=1em; vertical-align: -1.5em; display: inline-block;"/>'
         #return f'<img src="{svg}" style="display: inline-block;"/>'
         result = f"""<div style="display: inline-block;"> 
